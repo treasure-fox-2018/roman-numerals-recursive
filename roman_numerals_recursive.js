53216d4149a4294865a74784e98d71e3431f90cd
ronifajrian <ronifajrian@gmail.com>
2018-05-28 23:54:19 +0700

    add roman_rekursif

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,27 @@
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
+
+  var obj = {}
+  obj['M'] = 1000
+  obj['CM'] = 900
+  obj['D'] = 500
+  obj['CD'] = 400
+  obj['C'] = 100
+  obj['XC'] = 90
+  obj['L'] = 50
+  obj['XL'] = 40
+  obj['X'] = 10
+  obj['IX'] = 9
+  obj['V'] = 5
+  obj['IV'] = 4
+  obj['I'] = 1
+
+  for (const x in obj) {
+    if (input >= obj[x]) {
+      return x + toRoman(input - obj[x])
+    }
+  }
+  return ''
 }
 
 console.log('My totally sweet testing script for new roman\n')
