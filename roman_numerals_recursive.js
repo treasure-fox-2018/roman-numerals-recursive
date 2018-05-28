8d430f640c23b4711a93caf3777d5b2430baf0af
Tushar <tusharbedi7373@hotmail.com>
2018-05-28 15:53:22 +0700

    completed challenge roman recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,7 +1,19 @@
 function toRoman(input) {
-  // start your code here
-  return to_roman(input);
+  const decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
+
+  if (input === 0) {
+    return ''
+  } else {
+    for (let i = 0; i < decimal.length; i++) {
+      while (input >= decimal[i]) {
+        input -= decimal[i];
+        return roman[i] + toRoman(input)
+      }
+    }
+  }
 }
+
 
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
@@ -11,5 +23,6 @@ console.log('9     | IX       | ', toRoman(9))
 console.log('23    | XXIII    | ', toRoman(23))
 console.log('1453  | MCDLIII  | ', toRoman(1453))
 console.log('1646  | MDCXLVI  | ', toRoman(1646))
+
 
 module.exports = toRoman
