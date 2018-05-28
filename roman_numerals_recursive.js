f735bff70d50abcfa056b5c75db76c86ddc3d30f
Andre Sudi <y.andresudi@gmail.com>
2018-05-28 21:50:20 +0700

    roman rekursif

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,19 @@
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
+  var result = ''
+  var angka = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var romawi = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+  if (input === 0) {
+    return ''
+  } else {
+    for (var i = 0; i < angka.length; i++) {
+      while (input >= angka[i]) {
+        result = result + romawi[i]
+        return result + toRoman(input-angka[i])
+      }
+    }
+  }
 }
 
 console.log('My totally sweet testing script for new roman\n')
