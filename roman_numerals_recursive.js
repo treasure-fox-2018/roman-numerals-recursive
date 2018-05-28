e92ffcb4521db215decded30aa1824b7f1a67603
“Ollas2204” <fitrisakinah17@gmail.com>
2018-05-28 16:44:06 +0700

    roman_numerals_recureive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,15 @@
-function toRoman(input) {
+function toRoman(integer) {
   // start your code here
-  return to_roman(input);
+  let angkaRoman = [1, 4, 5, 9, 10, 40, 50, 100, 400, 500, 900, 1000];
+  let roman = ["I", "IV", "V", "IX", "X", "XL", "L", "C", "CD", "D", "CM", "M"]
+  if (integer == 0) {
+    return ""
+  }
+  for (var i = angkaRoman.length-1; i >=0; i--) {
+      if (angkaRoman[i]<=integer) {
+        return roman[i]+toRoman(integer-angkaRoman[i])
+      }
+    }
 }
 
 console.log('My totally sweet testing script for new roman\n')
