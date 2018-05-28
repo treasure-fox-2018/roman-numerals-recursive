0ab7d2d457f8ac9b5c2df08ee0c6680338a685bb
vinesto <oqvinesto@gmail.com>
2018-05-28 20:52:46 +0700

    First Commit

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,20 @@
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
+  var numArab = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  var numRoman = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+
+
+  var result=""
+  for(var i = 0; i<=numArab.length ; i++){
+    if(input === 0){
+      return ''
+    }else
+    if(numArab[i]<=input){
+      result += numRoman[i]
+      input = input - numArab[i]
+      return result + toRoman(input);
+    }
+  }
 }
 
 console.log('My totally sweet testing script for new roman\n')
