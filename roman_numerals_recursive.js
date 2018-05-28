7a03c2c6bef7479f7a756d4d03dce1b05108da4b
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2018-05-28 19:27:24 +0700

    Tugas Roman-numerals-recursive Fadli Al Baihaqi done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,35 @@
 function toRoman(input) {
+  var object = {
+    'M' : 1000,
+    'CM' :900,
+    'D' :500,
+    'CD' :400,
+    'C' : 100,
+    'XC' :90,
+    'L' : 50,
+    'XL' :40,
+    'X' : 10,
+    'IX' : 9,
+    'V' : 5,
+    'IV' : 4,
+    'I' : 1
+  }
+
+  var hasil = ''
+  if (input === 0) {
+    return hasil;
+  }
+  else {
+    for (var keys in object) {
+      if (input >= object[keys]) {
+        hasil = hasil + keys;
+        return hasil + toRoman(input-object[keys])
+      }
+      
+    }
+  }
   // start your code here
-  return to_roman(input);
+  // return to_roman(input);
 }
 
 console.log('My totally sweet testing script for new roman\n')
@@ -13,3 +42,36 @@ console.log('1453  | MCDLIII  | ', toRoman(1453))
 console.log('1646  | MDCXLVI  | ', toRoman(1646))
 
 module.exports = toRoman
+
+
+/*
+function to_roman (num) {
+  var object = {
+     'M' : 1000,
+     'CM' :900,
+     'D' :500,
+     'CD' :400,
+     'C' : 100,
+     'XC' :90,
+     'L' : 50,
+     'XL' :40,
+     'X' : 10,
+     'IX' : 9,
+     'V' : 5,
+     'IV' : 4,
+     'I' : 1
+  }
+  // console.log(object.a1)
+  var result = '';
+  
+
+  for (var keys in object) {
+    while (num >= object[keys]) {
+      result = result + keys;
+      num = num - object[keys];
+      
+    }
+  }
+  return result;
+}
+*/
