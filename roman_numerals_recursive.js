20ca8702aab671d96b3a3cc9978832318422afe5
fahrisyany <36616621+fahrisyany@users.noreply.github.com>
2018-05-28 23:40:34 +0700

    Update roman_numerals_recursive.js

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -2,31 +2,7 @@ function toRoman(num) {
   // start your code here
 
 
-  var romawi = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }
-  var result = ''
-
-  if (num === 0) {
-
-    return ' '
-
-  } else {
-
-    for (key in romawi) {
-      if (num >= romawi[key]) {
-        result += key
-        return result + toRoman(num - romawi[key])
-      }
-
-
-    }
-
-  }
-
-
-
-  // return toRoman(input);
 }
-console.log(toRoman(800))
 
 // console.log('My totally sweet testing script for new roman\n')
 // console.log('input | expected | actual')
