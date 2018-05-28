9f9e05fb3d3807cd283c355af2c00a6c6c0be1f6
shantidyah <shanti.dyah@gmail.com>
2018-05-28 18:16:07 +0700

    roman numerals rekursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,19 @@
 function toRoman(input) {
-  // start your code here
-  return to_roman(input);
+  // start your code
+  var roman = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"]
+  var arabic = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+
+  if (input==0) {
+    return ''
+  }
+  else {
+    for (var i = arabic.length-1; i>=0; i--) {
+      if (input>=arabic[i]) {
+        input-=arabic[i]
+        return roman[i]+toRoman(input);
+      }
+    }
+  }
 }
 
 console.log('My totally sweet testing script for new roman\n')
